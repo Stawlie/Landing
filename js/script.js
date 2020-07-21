624fccea32189cc258115a392a2cb07246bbd864
Stawlie <48334923+Stawlie@users.noreply.github.com>
2020-07-21 19:43:21 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,28 +2,33 @@
 
 let scroll;
 
+const headerBurger = document.querySelector('.header__burger'),
+    headerNav = document.querySelector('.header__nav'),
+    body = document.querySelector('body'),
+    header = document.querySelector('.header'),
+    headerWrapper = document.querySelector('.header__wrapper');
+
+
 window.addEventListener("scroll", () => {
     scroll = window.scrollY;
     if (scroll >= 50) {
+        headerWrapper.classList.add('fixed');
         header.classList.add('fixed');
     }
     else {
         header.classList.remove('fixed');
+        headerWrapper.classList.remove('fixed');
     }
 });
-
-const headerBurger = document.querySelector('.header__burger'),
-    headerNav = document.querySelector('.header__nav'),
-    body = document.querySelector('body'),
-    header = document.querySelector('.header');
-
 
 headerBurger.addEventListener('click', event => {
 
     if (!header.classList.contains('fixed')) {
         header.classList.add('fixed');
+        headerWrapper.classList.add('fixed');
     } else if (header.classList.contains('fixed') && scroll < 50) {
         header.classList.remove('fixed');
+        headerWrapper.classList.remove('fixed');
     }
 
     headerBurger.classList.toggle('active');
